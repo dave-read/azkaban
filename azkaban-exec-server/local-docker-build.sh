0f641ad8c243dbf561e6e4abd875746c62517770
dave-read <a@b.com>
2019-09-14 23:47:12 -0700

    add docker build files

---
+++ azkaban-exec-server/local-docker-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+PGM=$(basename $0)
+DIR=$(dirname $0)
+
+PROJECT=azkaban-exec-server
+
+# assumes gradle build with target installDist was run to produce folder:
+# $PROJECT/build/install/$PROJECT
+
+# path to Dockerfile
+DOCKER_FILE=$DIR/Dockerfile
+# path to build context
+BUILD_CONTEXT=$DIR/build/install/$PROJECT
+if [[ ! -d $BUILD_CONTEXT ]];then
+    echo "$PGM: Build context folder does not exist:$BUILD_CONTEXT. Make sure you build with installDist target."
+    exit 1
+fi
+docker build -t $PROJECT -f $DOCKER_FILE $BUILD_CONTEXT
